d2f0645f0178251e422b046def25101a997c9e09
Kaonuri <hon454@naver.com>
2017-03-16 18:39:34 +0900

    오토바이 소리 임포트

---
+++ Assets/Scripts/Event/TSEventLoadScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TSEventLoadScene : TSEvent
+{
+    [SerializeField] private string sceneName;
+
+    protected override void OnStart()
+    {
+        GameManager.Instacne.sceneManger.ChangeScene(sceneName);
+        QuitEvent();
+    }
+
+    protected override void OnQuit()
+    {
+    }
+}
