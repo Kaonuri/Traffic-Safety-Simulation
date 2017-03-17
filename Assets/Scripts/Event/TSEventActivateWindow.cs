94076e1f8161c45760cd804d6d783f2b72ad6691
Kaonuri <hon454@naver.com>
2017-03-17 14:33:49 +0900

    윈도우 페이드 구현

--- Assets/Scripts/Event/TSEventActivateWindow.cs
+++ Assets/Scripts/Event/TSEventActivateWindow.cs
@@ -1,17 +1,36 @@
 ﻿using UnityEngine;
+using Time = Ultimate.Time;
 
 public class TSEventActivateWindow : TSEvent
 {
     [SerializeField] private string hierachyName;
 
+    private Hierarchy hierarchy;
+    private CanvasGroup canvasGroup;
+
+    private float elapsedTime;
+    private float fadeDuration = 0.6f;
+
     protected override void OnStart()
     {
         GameManager.Instacne.windowManager.hierarchyManager.ActivateHierarchy(hierachyName);
-        QuitEvent();
+        canvasGroup = GameManager.Instacne.windowManager.hierarchyManager.GetHierarchy(hierachyName).GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0f;
+        elapsedTime = 0f;
+    }
+
+    private void Update()
+    {
+        canvasGroup.alpha += Time.globalDeltaTime / fadeDuration;
+        elapsedTime += Time.globalDeltaTime;
+
+        if (elapsedTime >= fadeDuration)
+        {
+            QuitEvent();
+        }
     }
 
     protected override void OnQuit()
     {
     }
 }
-
--- Assets/Scripts/Event/TSEventDeactivateWindow.cs
+++ Assets/Scripts/Event/TSEventDeactivateWindow.cs
@@ -1,13 +1,35 @@
 ﻿using UnityEngine;
+using Time = Ultimate.Time;
 
 public class TSEventDeactivateWindow : TSEvent
 {
     [SerializeField] private string hierachyName;
 
+    private Hierarchy hierarchy;
+    private CanvasGroup canvasGroup;
+
+    private float elapsedTime;
+    private float fadeDuration = 0.3f;
+
     protected override void OnStart()
     {
-        GameManager.Instacne.windowManager.hierarchyManager.DeactivateHierarchy(hierachyName);
-        QuitEvent();
+
+        canvasGroup = GameManager.Instacne.windowManager.hierarchyManager.GetHierarchy(hierachyName).GetComponent<CanvasGroup>();
+
+        canvasGroup.alpha = 1f;
+        elapsedTime = 0f;
+    }
+
+    private void Update()
+    {
+        canvasGroup.alpha -= Time.globalDeltaTime / fadeDuration;
+        elapsedTime += Time.globalDeltaTime;        
+
+        if (elapsedTime >= fadeDuration)
+        {
+            GameManager.Instacne.windowManager.hierarchyManager.DeactivateHierarchy(hierachyName);
+            QuitEvent();
+        }
     }
 
     protected override void OnQuit()
